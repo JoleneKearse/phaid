d7f3485993d1611f42bf9c14dcfbab82d4dc37bd
JoleneKearse <pharaohnutz@gmail.com>
2025-03-07 14:12:21 -0500

    feat: create checkboxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
+import Checkbox from "./components/Checkbox";
 
 function App() {
 
   return (
     <div className="py-20 w-1/2 mx-auto">
       <Header />
+      <Checkbox type="webp" />
+      <Checkbox type="rmBackground" />
+      <Checkbox type="addAltText" />
     </div>
   )
 }
---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,34 @@
+type CheckboxProps = {
+  type: string;
+}
+
+const Checkbox = ({ type }: CheckboxProps) => {
+  let labelText = "";
+  switch(type) {
+    case "webp":
+      labelText = "Convert to webp";
+      break;
+    case "rmBackground":
+      labelText = "Remove background";
+      break;
+    case "addAltText":
+      labelText = "Add alt text";
+      break;
+    default:
+      labelText = "";
+  }
+
+	return (
+		<div className="flex gap-4">
+			<input
+				type="checkbox"
+				name="photoOptions"
+				id={type}
+        value={type}
+			/>
+      <label htmlFor="photoOptions">{labelText}</label>
+		</div>
+	);
+};
+
+export default Checkbox;
